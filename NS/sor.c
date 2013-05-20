ab69b40bb68ccd57984bd41681f1aca911836b88
Roberto Camacho Barranco <crobertob@hotmail.com>
2013-05-20 15:37:15 +0200

    Updated SOR.C function
    ***CHECK IF THE BOUNDARY PRESSURE VALUES SHOULD BE SET THERE OR BEFORE
    OR AFTER********************
    In SOR, apart from the boundary values for the pressure at the boundary
    stripe, also the boundary values in the interior boundary cells should
    be set before each iteration step.
    In SOR, the iteration and the residual computation needs to be limited
    to the fluid cells. Besides, the normalization of the residual in SOR
    should be produced by dividing by the current number of fluid cells, and
    not by imax  jmax.

--- NS/sor.c
+++ NS/sor.c
@@ -1,49 +1,81 @@
 #include "sor.h"
 #include <math.h>
+#include "helper.h"
 
 void sor(
-         double omg,
-         double dx,
-         double dy,
-         int    imax,
-         int    jmax,
-         double **P,
-         double **RS,
-         double *res
-         ) {
-    int i,j;
-    double rloc;
-    double coeff = omg/(2.0*(1.0/(dx*dx)+1.0/(dy*dy)));
-    
-    /* SOR iteration */
-    for(i = 1; i <= imax; i++) {
-        for(j = 1; j<=jmax; j++) {
-            P[i][j] = (1.0-omg)*P[i][j]
-            + coeff*(( P[i+1][j]+P[i-1][j])/(dx*dx) + ( P[i][j+1]+P[i][j-1])/(dy*dy) - RS[i][j]);
-        }
-    }
-    
-    /* compute the residual */
-    rloc = 0;
-    for(i = 1; i <= imax; i++) {
-        for(j = 1; j <= jmax; j++) {
-            rloc += ( (P[i+1][j]-2.0*P[i][j]+P[i-1][j])/(dx*dx) + ( P[i][j+1]-2.0*P[i][j]+P[i][j-1])/(dy*dy) - RS[i][j])*
-            ( (P[i+1][j]-2.0*P[i][j]+P[i-1][j])/(dx*dx) + ( P[i][j+1]-2.0*P[i][j]+P[i][j-1])/(dy*dy) - RS[i][j]);
-        }
-    }
-    rloc = rloc/(imax*jmax);
-    rloc = sqrt(rloc);
-    /* set residual */
-    *res = rloc;
-    
-    
-    /* set boundary values */
-    for(i = 1; i <= imax; i++) {
-        P[i][0] = P[i][1];
-        P[i][jmax+1] = P[i][jmax];
-    }
-    for(j = 1; j <= jmax; j++) {
-        P[0][j] = P[1][j];
-        P[imax+1][j] = P[imax][j];
-    }
+		double omg,
+		double dx,
+		double dy,
+		int    imax,
+		int    jmax,
+		double **P,
+		double **RS,
+		double *res,
+		int **Flag
+) {
+	int i,j;
+	double rloc;
+	double coeff = omg/(2.0*(1.0/(dx*dx)+1.0/(dy*dy)));
+	int count = 0;
+
+	/* SOR iteration */
+	for(i = 1; i <= imax; i++) {
+		for(j = 1; j<=jmax; j++) {
+			if(Flag[i][j]>15){
+				P[i][j] = (1.0-omg)*P[i][j] + coeff*(( P[i+1][j]+P[i-1][j])/(dx*dx) +
+						( P[i][j+1]+P[i][j-1])/(dy*dy) - RS[i][j]);
+				count++;
+			}
+			else if(Flag[i][j]==B_N){
+				P[i][j]=P[i][j+1];
+			}
+			else if(Flag[i][j]==B_S){
+				P[i][j]=P[i][j-1];
+			}
+			else if(Flag[i][j]==B_W){
+				P[i][j]=P[i-1][j];
+			}
+			else if(Flag[i][j]==B_O){
+				P[i][j]=P[i+1][j];
+			}
+			else if(Flag[i][j]==B_NO){
+				P[i][j]=(P[i+1][j]+P[i][j+1])/2.0;
+			}
+			else if(Flag[i][j]==B_NW){
+				P[i][j]=(P[i][j+1]+P[i-1][j])/2.0;
+			}
+			else if(Flag[i][j]==B_SO){
+				P[i][j]=(P[i][j-1]+P[i+1][j])/2.0;
+			}
+			else if(Flag[i][j]==B_SW){
+				P[i][j]=(P[i][j-1]+P[i-1][j])/2.0;
+			}
+		}
+	}
+
+	/* compute the residual */
+	rloc = 0.0;
+	for(i = 1; i <= imax; i++) {
+		for(j = 1; j <= jmax; j++) {
+			if(Flag[i][j]>15){
+				rloc += ( (P[i+1][j]-2.0*P[i][j]+P[i-1][j])/(dx*dx) + ( P[i][j+1]-2.0*P[i][j]+P[i][j-1])/(dy*dy) - RS[i][j])*
+						( (P[i+1][j]-2.0*P[i][j]+P[i-1][j])/(dx*dx) + ( P[i][j+1]-2.0*P[i][j]+P[i][j-1])/(dy*dy) - RS[i][j]);
+			}
+		}
+	}
+	rloc = rloc/((double)count);
+	rloc = sqrt(rloc);
+	/* set residual */
+	*res = rloc;
+
+
+	/* set boundary values */
+	for(i = 1; i <= imax; i++) {
+		P[i][0] = P[i][1];
+		P[i][jmax+1] = P[i][jmax];
+	}
+	for(j = 1; j <= jmax; j++) {
+		P[0][j] = P[1][j];
+		P[imax+1][j] = P[imax][j];
+	}
 }
