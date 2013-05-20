7188cc803f9980b8d1f6d5a48608f6c83e58b711
Roberto Camacho Barranco <crobertob@hotmail.com>
2013-05-20 14:56:35 +0200

    Updated calculate_fg function to include boundary cells
    I changed some parts of the structure, instead of looking through the
    array twice (4 for loops), I changed it to just 2 and put a condition
    where I check the flag, just checking we don't go out of bounds, even
    though I'm not even sure if the conditions where correct in the first
    place. I also have a question here, should the F[0][0] and G[0][0]
    should be set to something? Maybe this is the part that the guy was
    telling us was wrong...also please check the conditions for the boundary
    cells.

--- NS/uvp.c
+++ NS/uvp.c
@@ -1,6 +1,7 @@
 #include "uvp.h"
 #include <math.h>
 #include <stdlib.h>
+#include "helper.h"
 
 /* ----------------------------------------------------------------------- */
 /*                             Function calculate_fg                       */
@@ -28,98 +29,115 @@
  *
  */
 void calculate_fg(
-                  double Re,
-                  double GX,
-                  double GY,
-                  double alpha,
-                  double dt,
-                  double dx,
-                  double dy,
-                  int imax,
-                  int jmax,
-                  double **U,
-                  double **V,
-                  double **F,
-                  double **G,
-                  int **Flag
-                  )
+		double Re,
+		double GX,
+		double GY,
+		double alpha,
+		double dt,
+		double dx,
+		double dy,
+		int imax,
+		int jmax,
+		double **U,
+		double **V,
+		double **F,
+		double **G,
+		int **Flag
+)
 
 {
-    
-    int i ;
-    int j ;
-    double du2dx ;
-    double duvdy ;
-    double d2udx2 ;
-    double d2udy2 ;
-    
-    double dv2dy ;
-    double duvdx ;
-    double d2vdx2 ;
-    double d2vdy2 ;
-        
-    /*Determines the value of F according to the formula above with the help of temporary variables*/
-    for ( i = 1 ; i < imax ; i++ )
-    {
-        for( j = 1 ; j <= jmax ; j ++ )
-        {
-            
-            d2udx2 = ( U[i+1][j]  - 2*U[i][j] + U[i-1][j] ) / ( dx * dx) ;
-            
-            d2udy2 = ( U[i][j+1]  - 2*U[i][j] + U[i][j-1]) / (dy * dy )  ;
-            
-            du2dx = (1/dx) * ( ( (U[i][j] + U[i+1][j])/2 )*( (U[i][j] + U[i+1][j])/2 ) - ( (U[i-1][j] + U[i][j])/2 )*( (U[i-1][j] + U[i][j])/2 ) ) +
-            alpha/dx * ( abs( U[i][j] + U[i+1][j] ) / 2  * ( U[i][j] - U[i+1][j] ) / 2 - abs( U[i-1][j] + U[i][j] ) / 2  * ( U[i-1][j] - U[i][j] ) / 2   ) ;
-            
-            duvdy = (1/dy) * ( ( V[i][j] + V[i+1][j] ) /2  *  ( U[i][j] + U[i][j+1] )/2 - (V[i][j-1] + V[i+1][j-1])/2 * (U[i][j-1] + U[i][j])/2  ) +
-            alpha/dy * (abs( V[i][j] + V[i+1][j] ) /2  *  ( U[i][j] - U[i][j+1] )/2 - abs(V[i][j-1] + V[i+1][j-1])/2 * (U[i][j-1] - U[i][j])/2 ) ;
-            
-            
-            
-            F[i][j] = U[i][j]  + dt * ( 1/Re * ( (d2udx2 ) + (d2udy2) ) - (du2dx)  - duvdy + GX ) ;
-            
-            
-        }
-    }
-    
-    /*Determines the value of G according to the formula above with the help of temporary variables*/
-    for ( i = 1 ; i <= imax ; i++ )
-    {
-        for( j = 1 ; j < jmax ; j ++ )
-        {
-            
-            d2vdx2 = ( V[i+1][j]  - 2*V[i][j] + V[i-1][j] ) / ( dx * dx) ;
-            
-            d2vdy2 = ( V[i][j+1]  - 2*V[i][j] + V[i][j-1]) / (dy * dy )  ;
-            
-            
-            duvdx = (1/dx) * ( ( V[i][j] + V[i+1][j] ) /2  *  ( U[i][j] + U[i][j+1] )/2 - (U[i-1][j] + U[i-1][j+1])/2 * (V[i-1][j] + V[i][j])/2  ) +
-            alpha/dx * (( V[i][j] - V[i+1][j] ) /2  *  abs( U[i][j] + U[i][j+1] )/2 - abs(U[i-1][j] + U[i-1][j+1])/2 * (V[i-1][j] - V[i][j])/2 ) ;
-            
-            dv2dy = (1/dy) * ( ( (V[i][j] + V[i][j+1])/2 )*( (V[i][j] + V[i][j+1])/2 ) - ( (V[i][j-1] + V[i][j])/2 )* (V[i][j-1] + V[i][j])/2 )  +
-            alpha/dy * ( abs( V[i][j] + V[i][j+1] ) / 2  * ( V[i][j] - V[i][j+1] ) / 2 - abs( V[i][j-1] + V[i][j] ) / 2  * (  V[i][j-1] - V[i][j]  ) / 2   ) ;
-            
-            
-            
-            G[i][j] = V[i][j]  + dt * ( 1/Re * ( (d2vdx2 ) + (d2vdy2) ) - (duvdx)  - dv2dy + GY ) ;
-            
-        }
-    }
-    
-    /*Set boundary values along the columns*/
-    for (j = 1; j <= jmax; j++){
-        /*F values on right and left boundaries*/
-        F[0][j] = U[0][j];
-        F[imax][j] = U[imax][j];
-    }
-    
-    /*Set boundary values along the rows*/
-    for (i = 1; i <= imax; i++){
-        /*G values on top and bottom boundaries*/
-        G[i][0] = V[i][0];
-        G[i][jmax] = V[i][jmax];
-    }
-    
+	int i ;
+	int j ;
+	double du2dx ;
+	double duvdy ;
+	double d2udx2 ;
+	double d2udy2 ;
+
+	double dv2dy ;
+	double duvdx ;
+	double d2vdx2 ;
+	double d2vdy2 ;
+
+	/*Determines the value of F according to the formula above with the help of temporary variables*/
+	for ( i = 1 ; i <= imax ; i++ )
+	{
+		for( j = 1 ; j <= jmax ; j++ )
+		{
+			if((Flag[i][j]>15||Flag[i][j]==B_N||Flag[i][j]== B_S )&& i<imax ){
+				d2udx2 = ( U[i+1][j]  - 2*U[i][j] + U[i-1][j] ) / ( dx * dx) ;
+
+				d2udy2 = ( U[i][j+1]  - 2*U[i][j] + U[i][j-1]) / (dy * dy )  ;
+
+				du2dx = (1/dx) * ( ( (U[i][j] + U[i+1][j])/2 )*( (U[i][j] + U[i+1][j])/2 ) - ( (U[i-1][j] + U[i][j])/2 )*( (U[i-1][j] + U[i][j])/2 ) ) +
+						alpha/dx * ( abs( U[i][j] + U[i+1][j] ) / 2  * ( U[i][j] - U[i+1][j] ) / 2 - abs( U[i-1][j] + U[i][j] ) / 2  * ( U[i-1][j] - U[i][j] ) / 2   ) ;
+
+				duvdy = (1/dy) * ( ( V[i][j] + V[i+1][j] ) /2  *  ( U[i][j] + U[i][j+1] )/2 - (V[i][j-1] + V[i+1][j-1])/2 * (U[i][j-1] + U[i][j])/2  ) +
+						alpha/dy * (abs( V[i][j] + V[i+1][j] ) /2  *  ( U[i][j] - U[i][j+1] )/2 - abs(V[i][j-1] + V[i+1][j-1])/2 * (U[i][j-1] - U[i][j])/2 ) ;
+
+				F[i][j] = U[i][j]  + dt * ( 1/Re * ( (d2udx2 ) + (d2udy2) ) - (du2dx)  - duvdy + GX ) ;
+			}
+			/*Determines the value of G according to the formula above with the help of temporary variables*/
+			if((Flag[i][j]>15||Flag[i][j]==B_W||Flag[i][j]==B_O) && j<jmax ){
+				d2vdx2 = ( V[i+1][j]  - 2*V[i][j] + V[i-1][j] ) / ( dx * dx) ;
+
+				d2vdy2 = ( V[i][j+1]  - 2*V[i][j] + V[i][j-1]) / (dy * dy )  ;
+
+				duvdx = (1/dx) * ( ( V[i][j] + V[i+1][j] ) /2  *  ( U[i][j] + U[i][j+1] )/2 - (U[i-1][j] + U[i-1][j+1])/2 * (V[i-1][j] + V[i][j])/2  ) +
+						alpha/dx * (( V[i][j] - V[i+1][j] ) /2  *  abs( U[i][j] + U[i][j+1] )/2 - abs(U[i-1][j] + U[i-1][j+1])/2 * (V[i-1][j] - V[i][j])/2 ) ;
+
+				dv2dy = (1/dy) * ( ( (V[i][j] + V[i][j+1])/2 )*( (V[i][j] + V[i][j+1])/2 ) - ( (V[i][j-1] + V[i][j])/2 )* (V[i][j-1] + V[i][j])/2 )  +
+						alpha/dy * ( abs( V[i][j] + V[i][j+1] ) / 2  * ( V[i][j] - V[i][j+1] ) / 2 - abs( V[i][j-1] + V[i][j] ) / 2  * (  V[i][j-1] - V[i][j]  ) / 2   ) ;
+
+				G[i][j] = V[i][j]  + dt * ( 1/Re * ( (d2vdx2 ) + (d2vdy2) ) - (duvdx)  - dv2dy + GY ) ;
+			}
+			if(Flag[i][j]==B_N && i<imax && j<jmax){
+				G[i][j]=V[i][j];
+			}
+			else if(Flag[i][j]==B_S && i<imax && j<jmax){
+				G[i][j-1]=V[i][j-1];
+			}
+			else if(Flag[i][j]==B_W && i<imax && j<jmax){
+				F[i-1][j]=U[i-1][j];
+			}
+			else if(Flag[i][j]==B_O && i<imax && j<jmax){
+				F[i][j]=U[i][j];
+			}
+			else if(Flag[i][j]==B_NO && i<imax && j<jmax){
+				F[i][j]=U[i][j];
+				G[i][j]=V[i][j];
+			}
+			else if(Flag[i][j]==B_NW && i<imax && j<jmax){
+				F[i-1][j]=U[i-1][j];
+				G[i][j]=V[i][j];
+			}
+			else if(Flag[i][j]==B_SO && i<imax && j<jmax){
+				F[i][j]=U[i][j];
+				G[i][j-1]=V[i][j-1];
+			}
+			else if(Flag[i][j]==B_SW && i<imax && j<jmax){
+				F[i-1][j]=U[i-1][j];
+				G[i][j-1]=V[i][j-1];
+			}
+		}
+	}
+
+	/**
+	 * SHOULDN'T THESE VALUES BE SET TO 0???? WE ARE NOT INITIALIZING 0 0
+	 */
+	/*Set boundary values along the columns*/
+	for (j = 1; j <= jmax; j++){
+		/*F values on right and left boundaries*/
+		F[0][j] = U[0][j];
+		F[imax][j] = U[imax][j];
+	}
+
+	/*Set boundary values along the rows*/
+	for (i = 1; i <= imax; i++){
+		/*G values on top and bottom boundaries*/
+		G[i][0] = V[i][0];
+		G[i][jmax] = V[i][jmax];
+	}
+
 }
 
 /* ----------------------------------------------------------------------- */
@@ -134,21 +152,21 @@ void calculate_fg(
  *
  */
 void calculate_rs(
-                  double dt,
-                  double dx,
-                  double dy,
-                  int imax,
-                  int jmax,
-                  double **F,
-                  double **G,
-                  double **RS
-                  ) {
+		double dt,
+		double dx,
+		double dy,
+		int imax,
+		int jmax,
+		double **F,
+		double **G,
+		double **RS
+) {
 	int i, j;
-    for(i = 1; i <= imax; i++) {
-        for(j = 1; j <= jmax; j++) {
-            RS[i][j] = 1 / dt*( (F[i][j]-F[i-1][j])/dx + (G[i][j]-G[i][j-1])/dy);
-        }
-    }
+	for(i = 1; i <= imax; i++) {
+		for(j = 1; j <= jmax; j++) {
+			RS[i][j] = 1 / dt*( (F[i][j]-F[i-1][j])/dx + (G[i][j]-G[i][j-1])/dy);
+		}
+	}
 }
 
 /* ----------------------------------------------------------------------- */
@@ -164,46 +182,46 @@ void calculate_rs(
  *
  */
 void calculate_dt(
-                  double Re,
-                  double tau,
-                  double *dt,
-                  double dx,
-                  double dy,
-                  int imax,
-                  int jmax,
-                  double **U,
-                  double **V
-                  ) {
-    /*calculates maximum absolute velocities in x and y direction*/
-    double umax=0, vmax=0;
-    double a,b,c;
-    int i, j;
-    for(i = 1; i <= imax; i++) {
-        for(j = 1; j<=jmax; j++) {
-            if(abs(U[i][j])>umax)
-                umax = abs(U[i][j]);
-            
-            if(abs(V[i][j])>vmax)
-                vmax = abs(V[i][j]);
-            
-        }
-    }
-    
-    /*Determines the minimum of dt according to stability criteria and multiply it by safety factor tau if tau is positive, otherwise uses the default value of dt*/
-    if (tau>0){
-        a = Re/(2.0*(1.0/(dx*dx)+1.0/(dy*dy)));
-        b = dx/umax;
-        c = dy/vmax;
-        if(a < b && a < c){
-            *dt = tau * a;
-        }
-        else if(b < a && b < c){
-            *dt = tau * b;
-        }
-        else{
-            *dt = tau * c;
-        }
-    }
+		double Re,
+		double tau,
+		double *dt,
+		double dx,
+		double dy,
+		int imax,
+		int jmax,
+		double **U,
+		double **V
+) {
+	/*calculates maximum absolute velocities in x and y direction*/
+	double umax=0, vmax=0;
+	double a,b,c;
+	int i, j;
+	for(i = 1; i <= imax; i++) {
+		for(j = 1; j<=jmax; j++) {
+			if(abs(U[i][j])>umax)
+				umax = abs(U[i][j]);
+
+			if(abs(V[i][j])>vmax)
+				vmax = abs(V[i][j]);
+
+		}
+	}
+
+	/*Determines the minimum of dt according to stability criteria and multiply it by safety factor tau if tau is positive, otherwise uses the default value of dt*/
+	if (tau>0){
+		a = Re/(2.0*(1.0/(dx*dx)+1.0/(dy*dy)));
+		b = dx/umax;
+		c = dy/vmax;
+		if(a < b && a < c){
+			*dt = tau * a;
+		}
+		else if(b < a && b < c){
+			*dt = tau * b;
+		}
+		else{
+			*dt = tau * c;
+		}
+	}
 }
 
 /* ----------------------------------------------------------------------- */
@@ -224,29 +242,29 @@ void calculate_dt(
  * @image html calculate_uv.jpg
  */
 void calculate_uv(
-                  double dt,
-                  double dx,
-                  double dy,
-                  int imax,
-                  int jmax,
-                  double **U,
-                  double **V,
-                  double **F,
-                  double **G,
-                  double **P
-                  ){
-    int i;
-    int j;
-    /*Calculate the new velocity U according to the formula above*/
-    for(i = 1; i <= (imax-1); i++){
-        for(j = 1; j <= jmax; j++){
-            U[i][j] = F[i][j]-(dt/dx)*(P[i+1][j]-P[i][j]);
-        }
-    }
-    /*Calculate the new velocity V according to the formula above*/
-    for(i = 1; i <= imax; i++){
-        for(j = 1; j <= (jmax-1); j++){
-            V[i][j] = G[i][j]-(dt/dy)*(P[i][j+1]-P[i][j]);
-        }
-    }
+		double dt,
+		double dx,
+		double dy,
+		int imax,
+		int jmax,
+		double **U,
+		double **V,
+		double **F,
+		double **G,
+		double **P
+){
+	int i;
+	int j;
+	/*Calculate the new velocity U according to the formula above*/
+	for(i = 1; i <= (imax-1); i++){
+		for(j = 1; j <= jmax; j++){
+			U[i][j] = F[i][j]-(dt/dx)*(P[i+1][j]-P[i][j]);
+		}
+	}
+	/*Calculate the new velocity V according to the formula above*/
+	for(i = 1; i <= imax; i++){
+		for(j = 1; j <= (jmax-1); j++){
+			V[i][j] = G[i][j]-(dt/dy)*(P[i][j+1]-P[i][j]);
+		}
+	}
 }
